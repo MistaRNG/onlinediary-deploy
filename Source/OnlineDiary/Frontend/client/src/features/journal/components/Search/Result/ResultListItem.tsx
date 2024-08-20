7dfc6a5f9af5554ee1c7c61b278360b506052f7e
robert.hobelsberger <robert.hobelsberger@stud.htwk-leipzig.de>
2024-08-20 22:37:24 +0200

    added delete button for journal entries

--- Source/OnlineDiary/Frontend/client/src/features/journal/components/Search/Result/ResultListItem.tsx
+++ Source/OnlineDiary/Frontend/client/src/features/journal/components/Search/Result/ResultListItem.tsx
@@ -1,7 +1,10 @@
-import { Card, Elevation } from "@blueprintjs/core";
+import { Card, Elevation, Button, Icon } from "@blueprintjs/core";
 import { useNavigate } from "react-router-dom";
 import { getResultDate } from "common/helpers";
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { deleteJournal } from "features/journal/journalSlice";
+import { AppDispatch } from "app/store";
 
 interface ResultListItemProps {
   date: string;
@@ -10,12 +13,35 @@ interface ResultListItemProps {
 
 const ResultListItem: React.FC<ResultListItemProps> = ({ date, title }) => {
   const navigate = useNavigate();
+  const dispatch: AppDispatch = useDispatch();
+
+  const [hovered, setHovered] = useState(false);
+
   const clickHandler = () => navigate(`/journal/${date}`);
 
+  const deleteHandler = (event: React.MouseEvent) => {
+    event.stopPropagation();
+
+    const confirmed = window.confirm("Are you sure you want to delete this journal entry?");
+    if (confirmed) {
+      dispatch(deleteJournal(date));
+    }
+  };
+
   return (
-    <Card onClick={clickHandler} interactive={true} elevation={Elevation.TWO}>
-      <h5>{getResultDate(date)}</h5>
-      <p>{title || "Untitled"}</p>
+    <Card onClick={clickHandler} interactive={true} elevation={Elevation.TWO} className="result-list-item">
+      <div className="result-content">
+        <h5>{getResultDate(date)}</h5>
+        <p>{title || "Untitled"}</p>
+      </div>
+      <Button 
+        icon={<Icon icon="trash" style={{ color: hovered ? '#fb3030' : '#fd5d5d' }} />} 
+        minimal={true} 
+        onClick={deleteHandler} 
+        className="delete-button"
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+      />
     </Card>
   );
 };
