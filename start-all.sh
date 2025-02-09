d8aadf8bf4441413a6e711b47147feded241e1b9
robert.hobelsberger <robert.hobelsberger@stud.htwk-leipzig.de>
2025-02-09 01:22:04 +0100

    deployment-anpassung

--- start-all.sh
+++ start-all.sh
@@ -1,8 +1,19 @@
-for service in auth-service comments-service journals-service likes-service mode-service users-service gateway-service; do
+#!/bin/bash
+
+declare -A services_ports=(
+    ["auth-service"]=3001
+    ["comments-service"]=3002
+    ["journals-service"]=3005
+    ["likes-service"]=3003
+    ["mode-service"]=3006
+    ["users-service"]=3004
+    ["gateway-service"]=4000
+)
+
+for service in "${!services_ports[@]}"; do
     service_path="/app/server/webservices/$service"
-    port_var="${service^^}_PORT"
-    service_port=$(eval echo "\$$port_var")
-    
+    service_port=${services_ports[$service]}
+
     if [ -f "$service_path/dist/index.js" ]; then
         echo "Starte $service auf Port $service_port..."
         cd "$service_path" && PORT=$service_port npm start &
@@ -13,8 +24,8 @@ done
 
 frontend_path="/app/client/build"
 if [ -d "$frontend_path" ]; then
-    echo "Starte Frontend auf Port $FRONTEND_PORT..."
-    cd /app/client && npx serve -s build -l $FRONTEND_PORT &
+    echo "Starte Frontend auf Port 3000..."
+    cd /app/client && npx serve -s build -l 3000 &
 else
     echo "Frontend-Build nicht gefunden! Bitte sicherstellen, dass das Frontend gebaut wurde."
 fi
