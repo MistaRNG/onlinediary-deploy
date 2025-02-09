46683abeebf0467039a40c8409258e7969c6e824
robert.hobelsberger <robert.hobelsberger@stud.htwk-leipzig.de>
2025-02-09 01:26:54 +0100

    deployment-anpassung

--- start-all.sh
+++ start-all.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-declare -A services_ports=(
-    ["auth-service"]=3001
-    ["comments-service"]=3002
-    ["journals-service"]=3005
-    ["likes-service"]=3003
-    ["mode-service"]=3006
-    ["users-service"]=3004
-    ["gateway-service"]=4000
-)
-
-for service in "${!services_ports[@]}"; do
-    service_path="/app/server/webservices/$service"
-    service_port=${services_ports[$service]}
+start_service() {
+    service_name=$1
+    service_port=$2
+    service_path="/app/server/webservices/$service_name"
 
     if [ -f "$service_path/dist/index.js" ]; then
-        echo "Starte $service auf Port $service_port..."
+        echo "Starte $service_name auf Port $service_port..."
         cd "$service_path" && PORT=$service_port npm start &
     else
-        echo "$service: dist/index.js nicht gefunden!"
+        echo "$service_name: dist/index.js nicht gefunden!"
     fi
-done
+}
+
+start_service "auth-service" 3001
+start_service "comments-service" 3002
+start_service "journals-service" 3005
+start_service "likes-service" 3003
+start_service "mode-service" 3006
+start_service "users-service" 3004
+start_service "gateway-service" 4000
 
 frontend_path="/app/client/build"
 if [ -d "$frontend_path" ]; then
